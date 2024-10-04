e2c906861fd9ddd2446f1217e70fa2e6dda9b4ac
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-10-04 11:23:09 +0300

    connected to database

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 //Main imports
 require('dotenv').config();
-
+const express = require('express');
 
 //Variables
 const TelegramBot = require('node-telegram-bot-api');
 const sequelize = require('./database');
 const webAppUrl = 'https://telegram-store.netlify.app';
 const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+
+//App
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+start();
 
 //Bot controlling
 bot.on('message', async (msg) => {
