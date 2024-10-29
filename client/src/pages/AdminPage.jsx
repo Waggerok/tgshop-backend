f133235974f056cf7de4cd941227670627897c32
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-10-29 16:51:43 +0300

    created all pages

---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminPage = () => {
+    return (
+        <>
+            Admin Page
+        </>
+    );
+};
+
+export default AdminPage;
---
+++ client/src/pages/DevicePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DevicePage = () => {
+    return (
+        <>
+            DevicePage
+        </>
+    );
+};
+
+export default DevicePage;
