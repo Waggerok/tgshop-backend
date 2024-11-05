03bf38f6fe4b8ba1c37977dfd5af420f50245b98
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-11-05 12:12:03 +0300

    added 2 methods: assignAdmin and assignUser in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,6 +38,42 @@ class UserController {
             res.status(500).json({ message: 'Ошибка при удалении пользователя', error })
         }
     }
+    async assignAdmin(req,res) {
+        try {
+            const {telegram_id} = req.params;
+
+            const user = await User.findOne({ where: {telegram_id} });
+            if (!user) {
+                return res.status(404).json({ message: 'Пользователь не найден' });
+            };
+
+            user.role = 'ADMIN';
+            await user.save();
+
+            return res.status(200).json({ message: `Роль администратора для ${telegram_id} успешно назначена` });
+        } catch (error) {
+            console.error('Error during assign administrator role', error);
+            res.status(500).json({ message: 'Ошибка при назначении роли администратора', error });
+        }
+    }
+    async assignUser(req,res) {
+        try {
+            const {telegram_id} = req.params;
+
+            const user = await User.findOne({ where: {telegram_id} });
+            if (!user) {
+                return res.status(404).json({ message: 'Пользователь не найден' });
+            };
+
+            user.role = 'USER';
+            await user.save();
+
+            return res.status(200).json({ message : `Роль пользователя для ${telegram_id} успешно назначена` });
+        } catch (error) {
+            console.error('Error during assign user role', error);
+            res.status(500).json({ message: 'Ошибка при назначении роли пользователя', error });
+        }
+    }
 }
 
 module.exports = new UserController();
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,8 @@ const router = new Router();
 const userController = require('../controllers/userController');
 
 router.post('/login', userController.authorizeUser);
-router.delete('/:telegram_id', userController.deleteUser);
+router.delete('/delete/:telegram_id', userController.deleteUser);
+router.put('/assignAdmin/:telegram_id', userController.assignAdmin);
+router.put('/assignUser/:telegram_id', userController.assignUser);
 
 module.exports = router;
