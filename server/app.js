19fc42fd2d8409c9b403907b3e6e87423929633c
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-11-12 14:37:08 +0800

    downloaded axios and use cors in app.js

--- server/app.js
+++ server/app.js
@@ -9,12 +9,14 @@ const orderRouter = require('./routes/orderRouter');
 const deviceRouter = require('./routes/deviceRouter');
 const basketRouter = require('./routes/basketRouter');
 const path = require('path');
+const cors = require('cors');
 
 //Variables
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //App
+app.use(cors());
 app.use(express.json());
 app.use('/uploads/images', express.static(path.join(__dirname, 'uploads/images')));
 app.use('/uploads/models', express.static(path.join(__dirname, 'uploads/models')));
