9bf05d466cb647ff8e3f373a28f0294bae5ec37c
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-11-24 13:15:43 +0300

    getting information for cards by database

--- client/src/components/UI/DeviceCard/DeviceCard.jsx
+++ client/src/components/UI/DeviceCard/DeviceCard.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import styles from './DeviceCard.module.css';
-import mac from '../../../img/macbook.jpg'
 
-const DeviceCard = () => {
+const DeviceCard = (props) => {
+
     return (
         <div className={styles.card}>
             <div className={styles.card__items}>
                 <div className={styles.card__items_image}>
-                    <img src={mac} alt="mac"/>
+                    <img src={props.image} alt={props.title}/>
                 </div>
                 <div className={styles.card__items_title}>
-                    <p>Macbook Pro M2 14` 16gb / 512 GB</p>
+                    <p>{props.title}</p>
                 </div>
                 <div className={styles.card__items_price}>
                     <p>
-                        <b>123098 РУБ.</b>
+                        <b>{props.price}РУБ.</b>
                     </p>
                 </div>
             </div>
-        </div>
+        </div>        
     );
 };
 
