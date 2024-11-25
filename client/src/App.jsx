7fdd19b729fe171a42c5821c65c500ad8dc059e6
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-11-25 14:14:57 +0300

    adding useeffect tg.ready and add script to html file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
 
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+    useEffect(() => {
+        tg.ready();
+    },[])
+
     return (
         <BrowserRouter>
             <Navbar/>
